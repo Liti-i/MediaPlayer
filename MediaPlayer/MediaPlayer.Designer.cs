c709ddb36c99b1ea5662386daf1474689b2e5d90
s1131430@mail.yzu.edu.tw <s1131430@mail.yzu.edu.tw>
2026-04-30 21:17:09 +0800

    加入專案檔案。

---
+++ MediaPlayer/MediaPlayer.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MediaPlayer
+{
+    partial class frmMediaPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMediaPlayer));
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.palButton = new System.Windows.Forms.Panel();
+            this.lblDuration = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.palButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(836, 546);
+            this.wmpVideo.TabIndex = 0;
+            this.wmpVideo.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnBrowser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.Location = new System.Drawing.Point(272, 15);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(123, 81);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = false;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(401, 15);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(123, 81);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.LightBlue;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(659, 15);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(123, 81);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnPause.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(530, 15);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(123, 81);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // palButton
+            // 
+            this.palButton.Controls.Add(this.lblDuration);
+            this.palButton.Controls.Add(this.btnBrowser);
+            this.palButton.Controls.Add(this.btnPause);
+            this.palButton.Controls.Add(this.btnPlay);
+            this.palButton.Controls.Add(this.btnStop);
+            this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.palButton.Location = new System.Drawing.Point(0, 425);
+            this.palButton.Name = "palButton";
+            this.palButton.Size = new System.Drawing.Size(836, 121);
+            this.palButton.TabIndex = 7;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblDuration.Location = new System.Drawing.Point(36, 34);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(67, 25);
+            this.lblDuration.TabIndex = 7;
+            this.lblDuration.Text = "           ";
+            // 
+            // frmMediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 546);
+            this.Controls.Add(this.palButton);
+            this.Controls.Add(this.wmpVideo);
+            this.Name = "frmMediaPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "多媒體播放器";
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.palButton.ResumeLayout(false);
+            this.palButton.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Panel palButton;
+        private System.Windows.Forms.Label lblDuration;
+    }
+}
+
---
+++ MediaPlayer/MediaPlayer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class frmMediaPlayer : Form
+    {
+        private const int WM_GETMINMAXINFO = 0x24;
+        private int videoWidth = 0;
+        private int videoHeight = 0;
+        private Timer playbackTimer;
+        private double totalDuration = 0;
+
+        public frmMediaPlayer()
+        {
+            InitializeComponent();
+            // 訂閱影片開啟完成事件
+            wmpVideo.OpenStateChange += WmpVideo_OpenStateChange;
+            
+            // 初始化計時器用於更新播放進度
+            playbackTimer = new Timer();
+            playbackTimer.Interval = 500; // 每500毫秒更新一次
+            playbackTimer.Tick += PlaybackTimer_Tick;
+        }
+
+        private void PlaybackTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // 取得當前播放時間
+                double currentPosition = wmpVideo.Ctlcontrols.currentPosition;
+
+                // 將秒數轉換為 時:分:秒 格式
+                int curHours = (int)(currentPosition / 3600);
+                int curMinutes = (int)((currentPosition % 3600) / 60);
+                int curSeconds = (int)(currentPosition % 60);
+
+                int totalHours = (int)(totalDuration / 3600);
+                int totalMinutes = (int)((totalDuration % 3600) / 60);
+                int totalSeconds = (int)(totalDuration % 60);
+
+                // 更新標籤顯示當前播放時間 / 總時長
+                lblDuration.Text = $"{curHours:D2}:{curMinutes:D2}:{curSeconds:D2} / {totalHours:D2}:{totalMinutes:D2}:{totalSeconds:D2}";
+            }
+            catch
+            {
+                // 忽略計時更新期間的錯誤
+            }
+        }
+
+        private void WmpVideo_OpenStateChange(object sender, AxWMPLib._WMPOCXEvents_OpenStateChangeEvent e)
+        {
+            // 當影片完全開啟時
+            if (e.newState == 13) // 13 = wmppMediaOpen
+            {
+                try
+                {
+                    // 取得影片的實際寬度和高度
+                    videoWidth = wmpVideo.currentMedia.imageSourceWidth;
+                    videoHeight = wmpVideo.currentMedia.imageSourceHeight;
+
+                    // 取得影片時長（以秒為單位）
+                    totalDuration = wmpVideo.currentMedia.duration;
+                    
+                    // 將秒數轉換為 時:分:秒 格式
+                    int hours = (int)(totalDuration / 3600);
+                    int minutes = (int)((totalDuration % 3600) / 60);
+                    int seconds = (int)(totalDuration % 60);
+                    
+                    // 更新標籤顯示時長
+                    lblDuration.Text = $"00:00:00 / {hours:D2}:{minutes:D2}:{seconds:D2}";
+                    
+                    // 啟動計時器以更新播放進度
+                    playbackTimer.Start();
+
+                    // 設定視窗最小尺寸（加上按鈕面板的高度）
+                    int minHeight = videoHeight + palButton.Height;
+                    this.MinimumSize = new Size(videoWidth, minHeight);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"錯誤：{ex.Message}", "載入影片失敗", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // 處理視窗最小尺寸限制
+            if (m.Msg == WM_GETMINMAXINFO)
+            {
+                if (videoWidth > 0 && videoHeight > 0)
+                {
+                    MINMAXINFO mmi = (MINMAXINFO)System.Runtime.InteropServices.Marshal.PtrToStructure(m.LParam, typeof(MINMAXINFO));
+                    int minHeight = videoHeight + palButton.Height;
+                    mmi.ptMinTrackSize = new Point(videoWidth, minHeight);
+                    System.Runtime.InteropServices.Marshal.StructureToPtr(mmi, m.LParam, true);
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public System.Drawing.Point ptReserved;
+            public System.Drawing.Point ptMaxSize;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Point ptMinTrackSize;
+            public System.Drawing.Point ptMaxTrackSize;
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4files(*.mp4) | *.mp4 | AVI files(*.avi) | *.avi | All files(*.*) | *.* ";
+                if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                wmpVideo.URL = ofd.FileName;
+                wmpVideo.Ctlcontrols.stop(); // 停止
+                playbackTimer.Stop(); // 停止計時器
+                
+                // 顯示上傳成功提示
+                MessageBox.Show(
+                    $"影片上傳成功！\n\n檔案名稱：{System.IO.Path.GetFileName(ofd.FileName)}\n\n請按播放按鈕開始播放",
+                    "上傳成功",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play(); // 播放
+            playbackTimer.Start(); // 啟動計時器
+        }
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause(); // 暫停
+            playbackTimer.Stop(); // 停止計時器
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop(); // 停止
+            playbackTimer.Stop(); // 停止計時器
+            lblDuration.Text = "         "; // 清空時長顯示
+        }
+    }
+}
